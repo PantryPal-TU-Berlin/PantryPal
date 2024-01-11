3cf83d335d4aeb91e147d02c8901b2929855014f
Alexander Ratnikov <alex@AlexandersMBP8.fritz.box>
2024-01-11 14:48:50 +0100

    with authIcon

--- frontend/pages/login/login.tsx
+++ frontend/pages/login/login.tsx
@@ -1,6 +1,6 @@
 import { NavBar } from "../../components/navbar/navbar.tsx"
 import { Footer } from "../../components/footer/footer.tsx"
-
+import { AuthIcon } from "auth";
 
 export default
 	<html>
@@ -10,6 +10,7 @@ export default
 		</head>
 		<body>
 			<NavBar/>
+			<AuthIcon style="float: right;" />
 			<Footer/>
 		</body>
 	</html>
