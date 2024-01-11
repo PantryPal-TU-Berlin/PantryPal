bbaa5319765862b241f8526d77aa5a3c983a9a94
Taschfin <taschfint@gmail.com>
2024-01-11 22:44:28 +0100

    fixed entrypoint

--- frontend/pages/login/login.tsx
+++ frontend/pages/login/login.tsx
@@ -1,14 +1,15 @@
 import { NavBar } from "../../components/navbar/navbar.tsx"
 import { Footer } from "../../components/footer/footer.tsx"
-//import { AuthIcon } from "auth";
+import { AuthIcon } from "auth";
 
 
 export default
 		<body>
+			<NavBar/>
 			<div class="loginBackground">
 				<div class="loginBlock">
 					<h1>Welcome to PantryPal</h1>
-					
+					<AuthIcon/>
 				</div>
 			</div>
 			<Footer/>
