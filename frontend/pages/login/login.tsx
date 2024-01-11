bd7e2f78eb5e60669919d51dc8b783c94e3bdd3a
Alexander Ratnikov <alex@AlexandersMBP8.fritz.box>
2024-01-11 15:08:28 +0100

    login gecleant

--- frontend/pages/login/login.tsx
+++ frontend/pages/login/login.tsx
@@ -3,15 +3,9 @@ import { Footer } from "../../components/footer/footer.tsx"
 import { AuthIcon } from "auth";
 
 export default
-	<html>
-		<head>
-			<title>PantryPal</title>
-			<link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css"/>
-		</head>
 		<body>
 			<NavBar/>
 			<AuthIcon style="float: right;" />
 			<Footer/>
 		</body>
-	</html>
 		
