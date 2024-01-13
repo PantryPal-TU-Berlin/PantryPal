d927c7a6a83d1543ed7579a244d3748c3f4bd856
taschfin <taschfint@gmail.com>
2024-01-13 20:22:53 +0100

    implemented newsletter section

--- frontend/components/footer/footer.tsx
+++ frontend/components/footer/footer.tsx
@@ -5,11 +5,26 @@ import { Component } from "uix/components/Component.ts";
 		<div class="footer-content">
 			<div class="news-letter">
 				<h2>Newsletter</h2>
-				<h2>Melde dich zum Newsletter an!</h2>
-				<input type="text" id="lname" name="lname"/>
+				<h2>Melde dich zum Newsletter an!</h2>			<input type="text" id="lname" name="lname"/>
 			</div>
 			<div class="footer-navigation">
+				
+					<ul>
+						<li>1</li>
+						<li>2</li>
+						<li>3</li>
+					</ul>
+			
 	
+				<h1> PantryPal </h1>
+
+				
+					<ul>
+						<li>1</li>
+						<li>2</li>
+						<li>3</li>
+					</ul>
+				
 			</div>
 			<div class="social-media-links">
 	
