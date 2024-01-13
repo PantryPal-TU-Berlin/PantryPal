bc6ee71095bde3684852ab22d013d82f5c89aab5
taschfin <taschfint@gmail.com>
2024-01-13 16:28:49 +0100

    minor changes

--- frontend/components/footer/footer.tsx
+++ frontend/components/footer/footer.tsx
@@ -4,7 +4,9 @@ import { Component } from "uix/components/Component.ts";
 	<footer id="footer">
 		<div class="footer-content">
 			<div class="news-letter">
-				
+				<h2>Newsletter</h2>
+				<h2>Melde dich zum Newsletter an!</h2>
+				<input type="text" id="lname" name="lname"/>
 			</div>
 			<div class="footer-navigation">
 	
