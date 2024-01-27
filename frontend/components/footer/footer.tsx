9f530ae615498fe418e80941888385adcb38eb06
taschfin <taschfint@gmail.com>
2024-01-27 12:28:58 +0100

    zwischenstand

--- frontend/components/footer/footer.tsx
+++ frontend/components/footer/footer.tsx
@@ -2,7 +2,25 @@ import { Component } from "uix/components/Component.ts";
 
 @template(
 	<footer id="footer">
-		
+		<div class="footer-content">
+			<div class="news-letter">
+				<h2>Newsletter</h2>
+				<h2>Melde dich zum Newsletter an!</h2>			
+				<input type="text" id="lname" name="lname"/>
+			</div>
+			<div class="footer-navigation">
+				<h1>PantryPal</h1>
+				<ul>
+					<li>About US</li>
+					<li>FAQ</li>
+					<li>Contact US</li>
+				</ul>			
+				
+			</div>
+			<div class="social-media-links">
+				
+			</div>
+		</div>
 	</footer>
 )
 
