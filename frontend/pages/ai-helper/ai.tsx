b4a67ac91e803a253a4a6e0dba2d1bfae9717650
taschfin <taschfint@gmail.com>
2024-01-28 12:17:05 +0100

    zwischenstand

--- frontend/pages/ai-helper/ai.tsx
+++ frontend/pages/ai-helper/ai.tsx
@@ -1,17 +1,27 @@
+import { NavBar } from "frontend/components/navbar/navbar.tsx";
+import { Footer } from "frontend/components/footer/footer.tsx";
 
 const Ai = template(() => (
-	<div>
-		<link rel="stylesheet" href="./ai.scss"/>
-		<h1>eric</h1>
-	</div>
+  <div>
+    <link rel="stylesheet" href="./ai.scss" />
+
+    <div class="main">
+      <NavBar />
+      <div>
+        <div class="left-side-bar"></div>
+        <div class="main-content"></div>
+        <div class="left-side-bar"></div>
+      </div>
+      <Footer />
+    </div>
+  </div>
 ));
 
-export default
-	<html>
-		<head>
-			
-		</head>
-		<body>
-			<Ai/>
-		</body>
-	</html>
+export default (
+  <html>
+    <head></head>
+    <body>
+      <Ai />
+    </body>
+  </html>
+);
