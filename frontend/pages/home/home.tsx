efbdba846e6b61296505de0935adfd732cd0f925
Alex <alex@MacBook-Pro-von-Alex.fritz.box>
2024-01-30 00:01:32 +0100

    nav bar and footer fixed

--- frontend/pages/home/home.tsx
+++ frontend/pages/home/home.tsx
@@ -4,26 +4,18 @@ import { Footer } from "frontend/components/footer/footer.tsx";
 const Home = template(() => (
     <div>
         <link rel="stylesheet" href="./home.scss" />
+        
         <div class="main-container">
-            <NavBar />
-            <Footer />
+        <NavBar />
         </div>
-        </div>
-        ));
+        <Footer />
+    </div>
+));
 
 
 export default (
         <html>
-            <head>
-                <link
-                    rel="stylesheet"
-                    href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.0.0-beta1/css/bootstrap.min.css"
-                />
-                <link
-                    rel="stylesheet"
-                    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
-                />
-            </head>
+        
             <body>
                 <Home />
             </body>
