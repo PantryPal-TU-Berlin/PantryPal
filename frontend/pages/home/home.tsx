c291d9ee113a57e3d8b3e6a67239a09748fc8274
Alex <alex@MacBook-Pro-von-Alex.fritz.box>
2024-01-30 03:40:05 +0100

    home page basic structure
    Co-authored-by: iJas13 jasraj13437@gmail.com
    Co-authored-by: AyhamIssat issatayham10@gmail.com

--- frontend/pages/home/home.tsx
+++ frontend/pages/home/home.tsx
@@ -5,8 +5,69 @@ const Home = template(() => (
     <div>
         <link rel="stylesheet" href="./home.scss" />
         
-        <div class="main-container">
         <NavBar />
+        <div class="main-container">
+       
+
+        <div class="horizontal-container">
+            <div class="top-div"> {/*Text + Kategorie */}
+                <div class="user-block"> {/* Hallo User & Was willst du heute zubereiten */}
+                    <h5 class="hallo-heading">
+                        Hallo Laura
+                    </h5>
+                    <p class="frage-text">
+                        Was willst du heute zubereiten?
+                    </p>
+                </div>
+
+                <div class="filter-block">  {/*Filter categories*/}
+                    <button class="sort-button">Sort by...</button>
+                    <button class="filter-button">Filter by...</button>
+                   
+                </div>
+            </div>
+            <div class="horizontal-scroller">
+                    <div class="media-element">pizza</div>
+                    <div class="media-element">pizza</div>
+                    <div class="media-element">pizza</div>
+                    <div class="media-element">pizza</div>
+                    <div class="media-element">pizza</div>
+                    <div class="media-element">pizza</div>
+                </div>
+        </div>
+
+
+        <div class="vertical-container">
+            <div class="top-div"> {/*Text + Kategorie */}
+                <div class="user-block"> {/* Hallo User & Was willst du heute zubereiten */}
+                    <h5 class="hallo-heading">
+                        Daily best repices
+                    </h5>
+                    <p class="frage-text">
+                        Die spezielle Auswahl nach deinen Vorlieben.
+                    </p>
+                </div>
+
+                <div class="category-block">  {/*Filter categories*/}
+                    <button class="category dairy">Dairy</button>
+                    <button class="category meat">Meat</button>
+                    <button class="category vegan">Vegan</button>
+                    <button class="category fish">Fish</button>
+                    <input class="search-field" type="text" placeholder="Suche Rezepte nach Name, Zutat und ..." id="fname"></input>
+                </div>
+            </div>
+            <div class="vertical-scroller">
+                    <div class="dish-element">spaghetti</div>
+                    <div class="dish-element">chicken korma</div>
+                    <div class="dish-element">pancakes</div>
+                    <div class="dish-element">salat</div>
+                    <div class="dish-element">hummer</div>
+                    <div class="dish-element">fatoush</div>
+                </div>
+                
+        </div>
+
+
         </div>
         <Footer />
     </div>
@@ -15,7 +76,6 @@ const Home = template(() => (
 
 export default (
         <html>
-        
             <body>
                 <Home />
             </body>
