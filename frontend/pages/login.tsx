cbb2dc96be196c6a4e505328fb2c2b9a001c4728
taschfin <taschfint@gmail.com>
2024-02-03 11:33:42 +0100

    minor changes

--- frontend/pages/login.tsx
+++ frontend/pages/login.tsx
@@ -1,15 +1,16 @@
-import { NavBar } from "../components/navbar/navbar.tsx"
+import { NavBar } from "../components/navbar/navbar.tsx";
 
-
-export default
-	<html>
-		<head>
-			<title>PantryPal</title>
-			<link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css"/>
-		</head>
-		<body>
-			<NavBar/>
-
-		</body>
-	</html>
-		
+export default (
+  <html>
+    <head>
+      <title>PantryPal</title>
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+      />
+    </head>
+    <body>
+      <NavBar />
+    </body>
+  </html>
+);
