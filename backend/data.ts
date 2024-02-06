d384cf9f974ae26c3c09506e5c4ffcbeba4982b0
Elean1402 <eric.lean.maier93@gmail.com>
2024-02-06 18:49:17 +0100

    recipePost can be created

--- backend/data.ts
+++ backend/data.ts
@@ -1,27 +1,34 @@
 import { Datex } from "datex-core-legacy/datex.ts";
-import { UIX } from "uix";
 
-interface RecipeData {
-	name: string;
-	ingredients: string[];
-	steps: string[];
-	rating: number;
-	difficulty: number;
-	creator: string;
+// optional: Datex.Runtime.OPTIONS.PROTECT_POINTERS = true;
+
+interface ProfilDaten {
+	anzahlRezepte: number,
+	topRezept: string,
+	bewertung: number,
+	follower: number,
+	land: string,
+	aktivität: boolean,
+	beschreibung: string,
 }
-const recipes: Record<string, Datex.ObjectRef<RecipeData>> = {};
-export function createRecipe(recipeName: string) {
-	console.log(`Creating database entry for ${recipeName}`);
-	recipes[recipeName] = $$(
-	{
-		name: recipeName,
-		ingredients: [],
-		steps: [],
-		rating: 0,
-		difficulty: 0,
-		creator: "Unknown"
-	} as RecipeData
-	);
 
-	return recipes[recipeName];
+const users: Record<string, Datex.ObjectRef<ProfilDaten>> = {};
+
+export function createProfil() {
+	const user = datex.meta.sender.main.toString();
+
+	if (!(user in users)) {
+		console.log(`Creating database entry for ${user}`);
+		users[user] = $$(
+			{anzahlRezepte: 0,
+				topRezept: "-",
+				bewertung: 0,
+				follower: 0,
+				land: "-",
+				aktivität: false,
+				beschreibung: "Ich bin neu hier"} as ProfilDaten
+		);
+	}
+
+	return users[user];
 }
--- backend/structs/recipePost.ts
+++ backend/structs/recipePost.ts
@@ -1,9 +1,29 @@
-import { Recipe } from "./recipe.ts";
+import { Recipe, createRecipe } from "./recipe.ts";
 import { User } from "./user.ts";
 
-export type recipePost = {
+export type RecipePost = {
   user: User;
   id: number;
   recipe: Recipe;
   date: string;
 };
+
+let postIndex = 0;
+const posts: Record<number, RecipePost> = {};
+
+export function createPost(user: User, recipeIndex: number) {
+  const recipe = createRecipe("Recipe Name", "Description", "Instruction");
+  if (!recipe) {
+    console.log(`Recipe with index ${recipeIndex} does not exist.`);
+    return null;
+  }
+  const date = new Date().toISOString();
+  const post: RecipePost = {
+    user: user,
+    id: postIndex++,
+    recipe: recipe,
+    date: date
+  };
+  posts[post.id] = post;
+  return post;
+}
