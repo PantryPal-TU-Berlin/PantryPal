7ac3b93cf82956150498b3b7ca42286708ca11c2
Alina Patratii <alina.patratii@campus.tu-berlin.de>
2024-02-14 06:37:33 +0100

    add home page to entrypoint

--- frontend/entrypoint.ts
+++ frontend/entrypoint.ts
@@ -13,5 +13,8 @@ export default {
   "/": import("./pages/home/home.tsx"),
   "/login": import("frontend/pages/login/login.tsx"),
   "/ai-helper": import("frontend/pages/ai-helper/ai.tsx"),
+  "/home": import("frontend/pages/home/home.tsx"),
+
+  // show frontend-rendered page on /frontend
   "/profil": import("./pages/profile/profile_template.tsx"),
 };
